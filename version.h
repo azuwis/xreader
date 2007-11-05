1a9ae225b0cc43b5aa08641a92c2358976abc498
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2007-11-05 08:37:04 +0000

    set version num
    git-svn-id: http://xreader.googlecode.com/svn/trunk@94 9e70b50a-5c3c-0410-8115-a7454348afc8

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define EREADER_VERSION_NUM 0x010509C0
+#define EREADER_VERSION_NUM 0x01060000
 #define EREADER_VERSION_SHORT "1.01"
 #define EREADER_VERSION_LONG "1.0.1.0 Built "  __TIME__ " " __DATE__ " (hrimfaxi ×ÔÖÆ°æ)"
 #define EREADER_VERSION_STR_SHORT "xReader " EREADER_VERSION_SHORT
