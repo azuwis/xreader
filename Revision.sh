c7ca10cf05efea90468f68a153ab320c62c42f3f
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2007-11-18 04:42:56 +0000

    Reversion.sh
    git-svn-id: http://xreader.googlecode.com/svn/trunk@146 9e70b50a-5c3c-0410-8115-a7454348afc8

---
+++ Revision.sh
@@ -0,0 +1,35 @@
+# $Id: Revision.sh 74 2007-10-20 10:46:39Z soarchin $
+
+# Copyright 2007 aeolusc
+
+# This file is part of eReader2.
+
+# eReader2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# eReader2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#!/bin/sh
+
+test "$1" && extra="-$1"
+
+svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
+test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
+test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null`
+test $svn_revision || svn_revision=UNKNOWN
+
+NEW_REVISION="#define REVISION \"${svn_revision}\""
+OLD_REVISION=`cat ./Main/Revision.h 2> /dev/null`
+
+# Update version.h only on revision changes to avoid spurious rebuilds
+if test "$NEW_REVISION" != "$OLD_REVISION"; then
+    echo "$NEW_REVISION" > ./Revision.h
+fi
