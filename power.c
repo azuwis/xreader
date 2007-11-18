595f625cd9aac4c4a514aa8a2ec8ff555ecd250f
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2007-11-18 04:26:39 +0000

    fix cpu <= 15 issuef
    git-svn-id: http://xreader.googlecode.com/svn/trunk@134 9e70b50a-5c3c-0410-8115-a7454348afc8

--- power.c
+++ power.c
@@ -2,17 +2,31 @@
 
 #include <string.h>
 #include <psppower.h>
+#include "xrPrx/xrPrx.h"
 #include "power.h"
+
+bool use_prx_power_save = false;
 			
 extern void power_set_clock(dword cpu, dword bus)
 {
-	if(cpu > 222 || bus > 111)
-		scePowerSetClockFrequency(cpu, cpu, bus);
-	else
-	{
-		scePowerSetClockFrequency(222, 222, 111);
-		scePowerSetCpuClockFrequency(cpu);
-		scePowerSetBusClockFrequency(bus);
+	if(use_prx_power_save) {
+		xrPlayerSetSpeed(cpu, bus);
+		// 15Mhz can't use xrPlayerSetSpeed
+		if(cpu <= 15) {
+			scePowerSetBusClockFrequency(95);
+			scePowerSetCpuClockFrequency(cpu);
+			scePowerSetBusClockFrequency(bus);
+		}
+	}
+	else {
+		if(cpu > 222 || bus > 111)
+			scePowerSetClockFrequency(cpu, cpu, bus);
+		else
+		{
+			scePowerSetClockFrequency(222, 222, 111);
+			scePowerSetCpuClockFrequency(cpu);
+			scePowerSetBusClockFrequency(bus);
+		}
 	}
 }
 
