d54e04599a78d832daa3b632545b1f273e9d692c
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2007-12-02 13:46:57 +0000

    ready to use ME playback
    git-svn-id: http://xreader.googlecode.com/svn/trunk@202 9e70b50a-5c3c-0410-8115-a7454348afc8

--- scene.c
+++ scene.c
@@ -3666,7 +3666,7 @@ extern void scene_init()
 	strcat(mp3conf, "music.lst");
 	if(config.confver < 0x00090100 || !mp3_list_load(mp3conf)) {
 		sceRtcGetCurrentTick(&dbglasttick);
-//		mp3_list_add_dir("ms0:/MUSIC/");
+		mp3_list_add_dir("ms0:/MUSIC/");
 		sceRtcGetCurrentTick(&dbgnow);
 		dbg_printf(d, "mp3_list_add_dir(): %.2fs", pspDiffTime(&dbgnow, &dbglasttick));
 	}
@@ -3743,9 +3743,11 @@ extern void scene_exit()
 
 #ifdef ENABLE_MUSIC
 	mp3_end();
+#ifdef ENABLE_ME
 	extern void *pMMgr;
 	MusicMgrRelease(pMMgr);
 	DirRelease();
+#endif
 	char mp3conf[256];
 	strcpy(mp3conf, appdir);
 	strcat(mp3conf, "music.lst");
