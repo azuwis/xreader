30f56e81ee58cd0cd3d1d51153c01305f6543055
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2008-01-26 05:17:42 +0000

    rc9
    git-svn-id: http://xreader.googlecode.com/svn/trunk@268 9e70b50a-5c3c-0410-8115-a7454348afc8

--- fonts/releasefont.bat
+++ fonts/releasefont.bat
@@ -1,3 +1,4 @@
-zip fonts.zip ASC10 ASC12 ASC14 ASC16 GBK10 GBK12 GBK14 GBK16
+zip fonts.zip ASC10 ASC12 ASC14 ASC16 ASC18 GBK10 GBK12 GBK14 GBK16 GBK18
+:zip fonts.zip ASC12 GBK12 
 del ..\fonts.zip
 move fonts.zip .. 
