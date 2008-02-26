c0a8b8460f1410ee0589fcd99d4b3bb4f0d5f28e
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2008-02-26 19:30:10 +0000

    release updated
    git-svn-id: http://xreader.googlecode.com/svn/trunk@374 9e70b50a-5c3c-0410-8115-a7454348afc8

--- rel.sh
+++ rel.sh
@@ -5,4 +5,6 @@ cp EBOOT.PBP ./PSP/GAME/xReader
 cp bg.png ./PSP/GAME/xReader
 cp ./fonts/fonts.zip ./PSP/GAME/xReader
 cp ./xrPrx/xrPrx.prx ./PSP/GAME/xReader
+cp Changelog.txt ./PSP/GAME/xReader
+cp Readme.txt ./PSP/GAME/xReader
 rar a -v1500k xReader.rar PSP
