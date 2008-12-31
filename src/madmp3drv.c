005b55a0c82eec0bdede362d7991671d48b24f47
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2008-12-31 13:12:37 +0000

    rewrite fast forward code
    git-svn-id: http://xreader.googlecode.com/svn/branches/trunk-alpha-me@956 9e70b50a-5c3c-0410-8115-a7454348afc8

--- src/madmp3drv.c
+++ src/madmp3drv.c
@@ -228,6 +228,124 @@ static int madmp3_seek_seconds_offset_brute(double offset)
 	return 0;
 }
 
+/* Steal from libid3tag */
+
+enum tagtype
+{
+	TAGTYPE_NONE = 0,
+	TAGTYPE_ID3V1,
+	TAGTYPE_ID3V2,
+	TAGTYPE_ID3V2_FOOTER
+};
+
+enum
+{
+	ID3_TAG_FLAG_UNSYNCHRONISATION = 0x80,
+	ID3_TAG_FLAG_EXTENDEDHEADER = 0x40,
+	ID3_TAG_FLAG_EXPERIMENTALINDICATOR = 0x20,
+	ID3_TAG_FLAG_FOOTERPRESENT = 0x10,
+
+	ID3_TAG_FLAG_KNOWNFLAGS = 0xf0
+};
+
+typedef uint8_t id3_byte_t;
+typedef int id3_length_t;
+
+static enum tagtype tagtype(id3_byte_t const *data, id3_length_t length)
+{
+	if (length >= 3 && data[0] == 'T' && data[1] == 'A' && data[2] == 'G')
+		return TAGTYPE_ID3V1;
+
+	if (length >= 10 &&
+		((data[0] == 'I' && data[1] == 'D' && data[2] == '3') ||
+		 (data[0] == '3' && data[1] == 'D' && data[2] == 'I')) &&
+		data[3] < 0xff && data[4] < 0xff &&
+		data[6] < 0x80 && data[7] < 0x80 && data[8] < 0x80 && data[9] < 0x80)
+		return data[0] == 'I' ? TAGTYPE_ID3V2 : TAGTYPE_ID3V2_FOOTER;
+
+	return TAGTYPE_NONE;
+}
+
+unsigned long id3_parse_uint(id3_byte_t const **ptr, unsigned int bytes)
+{
+	unsigned long value = 0;
+
+	assert(bytes >= 1 && bytes <= 4);
+
+	switch (bytes) {
+		case 4:
+			value = (value << 8) | *(*ptr)++;
+		case 3:
+			value = (value << 8) | *(*ptr)++;
+		case 2:
+			value = (value << 8) | *(*ptr)++;
+		case 1:
+			value = (value << 8) | *(*ptr)++;
+	}
+
+	return value;
+}
+
+unsigned long id3_parse_syncsafe(id3_byte_t const **ptr, unsigned int bytes)
+{
+	unsigned long value = 0;
+
+	assert(bytes == 4 || bytes == 5);
+
+	switch (bytes) {
+		case 5:
+			value = (value << 4) | (*(*ptr)++ & 0x0f);
+		case 4:
+			value = (value << 7) | (*(*ptr)++ & 0x7f);
+			value = (value << 7) | (*(*ptr)++ & 0x7f);
+			value = (value << 7) | (*(*ptr)++ & 0x7f);
+			value = (value << 7) | (*(*ptr)++ & 0x7f);
+	}
+
+	return value;
+}
+
+static void parse_header(id3_byte_t const **ptr, unsigned int *version,
+						 int *flags, id3_length_t * size)
+{
+	*ptr += 3;
+
+	*version = id3_parse_uint(ptr, 2);
+	*flags = id3_parse_uint(ptr, 1);
+	*size = id3_parse_syncsafe(ptr, 4);
+}
+
+signed long id3_tag_query(id3_byte_t const *data, id3_length_t length)
+{
+	unsigned int version;
+	int flags;
+	id3_length_t size;
+
+	assert(data);
+
+	switch (tagtype(data, length)) {
+		case TAGTYPE_ID3V1:
+			return 128;
+
+		case TAGTYPE_ID3V2:
+			parse_header(&data, &version, &flags, &size);
+
+			if (flags & ID3_TAG_FLAG_FOOTERPRESENT)
+				size += 10;
+
+			return 10 + size;
+
+		case TAGTYPE_ID3V2_FOOTER:
+			parse_header(&data, &version, &flags, &size);
+			return -size - 10;
+
+		case TAGTYPE_NONE:
+			break;
+	}
+
+	return 0;
+}
+
 static int madmp3_seek_seconds_offset(double offset)
 {
 	uint32_t target_frame = abs(offset) * mp3info.average_bitrate / 8;
@@ -284,21 +402,16 @@ static int madmp3_seek_seconds_offset(double offset)
 				stream.error = 0;
 			}
 
-			ret = mad_frame_decode(&frame, &stream);
-
-			if (ret == -1) {
-				if (stream.error == MAD_ERROR_BUFLEN) {
-					continue;
-				}
-
-				if (MAD_RECOVERABLE(stream.error)) {
-					mad_stream_skip(&stream, 1);
-				} else {
+			if ((ret = mad_header_decode(&frame.header, &stream)) == -1) {
+				if (!MAD_RECOVERABLE(stream.error)) {
 					__end();
 					return -1;
 				}
+			} else {
+				ret = 0;
+				stream.error = 0;
 			}
-		} while (!(ret == 0 && stream.error == 0));
+		} while (!(ret == 0 && stream.sync == 1));
 		dbg_printf(d, "%s: tried %d times", __func__, cnt);
 
 		if (data.size > 0) {
@@ -419,8 +532,24 @@ static void madmp3_audiocallback(void *buf, unsigned int reqn, void *pdata)
 				if (MAD_RECOVERABLE(stream.error)
 					|| stream.error == MAD_ERROR_BUFLEN) {
 					if (stream.error == MAD_ERROR_LOSTSYNC) {
-						mad_stream_skip(&stream, 1);
+						long tagsize = id3_tag_query(stream.this_frame,
+													 stream.bufend -
+													 stream.this_frame);
+
+						if (tagsize > 0) {
+							mad_stream_skip(&stream, tagsize);
+						}
+
+						if (mad_header_decode(&frame.header, &stream) == -1) {
+							if (!MAD_RECOVERABLE(stream.error)) {
+								__end();
+								return;
+							}
+						} else {
+							stream.error = MAD_ERROR_NONE;
+						}
 					}
+
 					g_buff_frame_size = 0;
 					g_buff_frame_start = 0;
 					continue;
@@ -432,6 +561,10 @@ static void madmp3_audiocallback(void *buf, unsigned int reqn, void *pdata)
 
 			unsigned i;
 			uint16_t *output = &g_buff[0];
+
+			if (stream.error != MAD_ERROR_NONE) {
+				continue;
+			}
 
 			mad_synth_frame(&synth, &frame);
 			for (i = 0; i < synth.pcm.length; i++) {
