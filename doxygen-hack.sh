33c8073e340ccb02c8ceeebb183b779cb4c7eccc
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2009-04-28 14:20:38 +0000

    fix doxygen
    git-svn-id: http://xreader.googlecode.com/svn/branches/trunk-alpha-me@1099 9e70b50a-5c3c-0410-8115-a7454348afc8

--- doxygen-hack.sh
+++ doxygen-hack.sh
@@ -4,6 +4,6 @@ cd $1
 
 for file in *.html
 do
-	sed 's/<meta http-equiv="Content-Type" content="text\/html;charset=iso-8859-1">/<meta http-equiv="Content-Type" content="text\/html;charset=gbk">/g' "$file" > .t
+	sed 's/<meta http-equiv="Content-Type" content="text\/html;charset=.*">/<meta http-equiv="Content-Type" content="text\/html;charset=gbk">/g' "$file" > .t
 	mv -f .t "$file"
 done
