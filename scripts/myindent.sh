8ada4056bf5df2190c2e143d83f022c714d60ed9
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2009-05-29 07:48:31 +0000

    remove -bad option
    git-svn-id: http://xreader.googlecode.com/svn/branches/trunk-alpha-me@1266 9e70b50a-5c3c-0410-8115-a7454348afc8

--- scripts/myindent.sh
+++ scripts/myindent.sh
@@ -8,15 +8,15 @@ fi
 while [ $# -gt 0 ];
 do
 	SRC="$1"
-	INDENTFLAG="-kr -ut -bad -bap -br -bls -brs -ts4 -l80 -sob -cdw -cli4 -bls -i4 -nce"
+	INDENTFLAG="-kr -ut -bap -br -bls -brs -ts4 -l80 -sob -cdw -cli4 -bls -i4 -nce"
 	echo "Indenting $SRC"
 	test ! -f "$SRC" && (echo "$SRC doesn't exists"; shift; continue)
 #	dos2unix -c ISO "$SRC"
 	cat "$SRC" | tr -d "\r" > tmp.txt
 	mv tmp.txt "$SRC"
-	INDENTFLAG="-kr -ut -bad -bap -br -bls -brs -ts4 -l80 -sob -cdw -cli4 -bls -i4 -nce"
+	INDENTFLAG="-kr -ut -bap -br -bls -brs -ts4 -l80 -sob -cdw -cli4 -bls -i4 -nce"
 	indent $INDENTFLAG "$SRC"
-	INDENTFLAG="-kr -ut -bad -bap -br -bls -brs -ts4 -l80 -sob -cdw -cli4 -bls -i4 -ce"
+	INDENTFLAG="-kr -ut -bap -br -bls -brs -ts4 -l80 -sob -cdw -cli4 -bls -i4 -ce"
 	indent $INDENTFLAG "$SRC"
 #	dos2unix -c ISO "$SRC"
 	cat "$SRC" | tr -d "\r" > tmp.txt
