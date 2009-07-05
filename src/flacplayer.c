d770b11ba7875267718ef8230ef1f5423e7e0202
outmatch <outmatch@9e70b50a-5c3c-0410-8115-a7454348afc8>
2009-07-05 14:30:05 +0000

    update flac freq to 96
    git-svn-id: http://xreader.googlecode.com/svn/branches/trunk-alpha-me@1371 9e70b50a-5c3c-0410-8115-a7454348afc8

--- src/flacplayer.c
+++ src/flacplayer.c
@@ -785,7 +785,7 @@ static int flac_get_info(struct music_info *pinfo)
 		pinfo->cur_time = g_play_time;
 	}
 	if (pinfo->type & MD_GET_CPUFREQ) {
-		pinfo->psp_freq[0] = 66;
+		pinfo->psp_freq[0] = 96;
 		pinfo->psp_freq[1] = 111;
 	}
 	if (pinfo->type & MD_GET_INSKBPS) {
